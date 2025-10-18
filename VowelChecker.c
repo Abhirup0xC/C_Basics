7333f5433309b221efc048cedac705ac8d335aee
Abhirup <empireabhirup@gmail.com>
2025-10-19 03:27:25 +0530

    Implementing vowel checker

--- VowelChecker.c
+++ VowelChecker.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+int main() {
+    char a;
+    printf("Enter your character for checking: ");
+    scanf("%c", &a); 
+    switch(a)
+    {
+    	case 'a':
+    	case 'e':
+    	case 'i':
+    	case 'o':
+    	case 'u':
+    	case 'A':
+    	case 'E':
+    	case 'O':
+    	case 'I':
+    	case 'U': printf("The entered character is vowel!");
+		break;
+    	default : printf("The entered character is a consonent!");
+    }
+    return 0;   	
+}
