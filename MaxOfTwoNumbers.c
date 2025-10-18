fed1ab0a79c1ef7d8be0a01fb8b76fa1eb384ee0
Abhirup <empireabhirup@gmail.com>
2025-10-19 03:59:43 +0530

    To find maximum of two numbers

---
+++ MaxOfTwoNumbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a, b , c;
+	printf("Enter first number:");
+	scanf("%d", &a);
+	printf("Enter second number:");
+	scanf("%d", &b);
+	a>b? printf("%d is greater", a) : printf("%d is greater", b);
+	return 0;
+}
